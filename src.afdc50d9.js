db502f66c37784c051621ea6e46033dff546635d
OksanaMosendz <77731512+OksanaMosendz@users.noreply.github.com>
2021-06-27 20:45:42 +0000

    Deploying to gh-pages from @ OksanaMosendz/goit-js-hw-10-food-service@376d43a7ac3eaaf3968a82d62cf21b0dc26afd3f 🚀

--- src.afdc50d9.js
+++ src.afdc50d9.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10-food-service/src.b7a526bb.js.map
+//# sourceMappingURL=/goit-js-hw-10-food-service/src.afdc50d9.js.map
