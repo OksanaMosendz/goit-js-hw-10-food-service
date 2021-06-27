71c727db9f136b238f9ddbb4a773b1520a4afe30
OksanaMosendz <bagheera03@gmail.com>
2021-06-28 01:21:42 +0300

    create menu

---
+++ src/js/create-menu.js
@@ -0,0 +1,10 @@
+import menuJson from '../data/menu.json';
+
+import menuTemplate from '../template/menuTemplate.hbs';
+
+const menuList=document.querySelector('.js-menu');
+
+const  createMenu=(json, template, ul)=>
+  {ul.innerHTML=template(json);}
+
+  createMenu(menuJson, menuTemplate, menuList);
