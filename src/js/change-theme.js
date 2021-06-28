be83d66ae13c776ed5c8d10c8d2c294eea6521df
OksanaMosendz <bagheera03@gmail.com>
2021-06-28 10:21:44 +0300

    set theme

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -2,11 +2,23 @@ import {Theme} from './thems.js';
 const body= document.querySelector('body');
 const themeSwitchToggle=document.querySelector(`#theme-switch-toggle`);
 
-body.classList.add(Theme.LIGHT);
+const currentTheme=localStorage.getItem("currentTheme");
+body.classList.add(currentTheme);
 
-const changeTheme=()=>{ if(isLight)
+const changeTheme=()=>{
+  if  (body.classList.contains(Theme.LIGHT)){
+  body.classList.remove(Theme.LIGHT);
   body.classList.add(Theme.DARK);
+  localStorage.setItem("currentTheme", `${Theme.DARK}`);
 }
-
+  else 
+   if(body.classList.contains(Theme.DARK))
+   {
+    body.classList.remove(Theme.DARK);
+    body.classList.add(Theme.LIGHT);
+    localStorage.setItem("currentTheme", `${Theme.LIGHT}`);
+  }
+ }
+ 
 themeSwitchToggle.addEventListener('change',changeTheme);
 
